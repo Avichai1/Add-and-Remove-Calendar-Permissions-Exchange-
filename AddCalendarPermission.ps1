8f6b461f7c94f44bd920a7d11bb0fe644c24e3ba
Avichai1 <68597348+Avichai1@users.noreply.github.com>
2021-10-07 16:54:47 +0300

    Add files via upload

---
+++ AddCalendarPermission.ps1
@@ -0,0 +1,13 @@
+﻿$ExchangeServer=""
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://+$ExchangeServer+/Powershell/ -Authentication Kerberos
+Import-PSSession $Session -AllowClobber
+$User_Giving_Permissions = 'anniel@maof-group.co.il' #בעל הלוח שנה
+$User_Getting_Permissions = 'annaa@maof-group.co.il' #למי לתת הרשאות
+$CalendarFolder = Get-MailboxFolderStatistics -Identity $User_Giving_Permissions -FolderScope Calendar | Where-Object {$_.FolderType -eq 'Calendar'} | Select-Object Name, FolderId
+$path = "$User_Giving_Permissions"+":$($calendarFolder.FolderId)" 
+Get-MailboxFolderPermission -Identity "$path"
+Add-MailboxFolderPermission -Identity "$path" -User $User_Getting_Permissions -AccessRights Author
+Set-MailboxFolderPermission -Identity "$path" -User $User_Getting_Permissions -AccessRights Author
+$CalendarFolder = Get-MailboxFolderStatistics -Identity $User_Giving_Permissions -FolderScope Calendar | Where-Object {$_.FolderType -eq 'Calendar'} | Select-Object Name, FolderId
+Get-MailboxFolderPermission -Identity "$path"  
+Remove-PSSession $Session
---
+++ RemoveCalendarPermission.ps1
@@ -0,0 +1,15 @@
+﻿$ExchangeServer=""
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://+$ExchangeServer+/Powershell/ -Authentication Kerberos
+Import-PSSession $Session -AllowClobber
+$User_Giving_Permissions = @("", "", "") #בעל הלוח שנה
+$User_Getting_Permissions = "" #למי לתת הרשאות
+foreach($User_Giving in $User_Giving_Permissions)
+{
+   $CalendarFolder = Get-MailboxFolderStatistics -Identity $User_Giving -FolderScope Calendar | Where-Object {$_.FolderType -eq 'Calendar'} | Select-Object Name, FolderId
+   $path = "$User_Giving"+":$($calendarFolder.FolderId)" 
+   Get-MailboxFolderPermission -Identity "$path"
+   Remove-MailboxFolderPermission -Identity "$path"
+}
+$CalendarFolder = Get-MailboxFolderStatistics -Identity $User_Giving_Permissions -FolderScope Calendar | Where-Object {$_.FolderType -eq 'Calendar'} | Select-Object Name, FolderId
+Get-MailboxFolderPermission -Identity "$path"  
+Remove-PSSession $Session
